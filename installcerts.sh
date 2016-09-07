4818435c99ee0f7a2952fce995ebc6527874961c
Kemyke <kemyyy@gmail.com>
2016-09-07 16:21:19 +0200

    Add files via upload

--- installcerts.sh
+++ installcerts.sh
@@ -3,6 +3,7 @@
 # Github : kemyke
 # Version : 0.1
 
+#Add self signed certificates
 mkdir /certs
 mount -t cifs //nexogencontent.file.core.windows.net/contents/certs /certs -o vers=3.0,username=nexogencontent,password=jVWE/tNDWXTmLzxC6xzhS6bfreuUI4Q6WMjHhYjehnOq4x3SFt3ugozGWdgUBL7Bqffro1G0lwXCbOrNx1swMg==,dir_mode=0777,file_mode=0777
 update-ca-trust force-enable
@@ -17,3 +18,13 @@ service docker start
 rm /ca.crt
 umount /certs
 rm /certs -r
+
+#Add dockerrepo to host file
+echo '52.169.93.153   dockerrepo.nexogen.hu' >> /etc/hosts
+
+#Install mono
+mkdir /mono
+wget http://download.mono-project.com/repo/xamarin.gpg -P /mono
+rpm --import /mono/xamarin.gpg
+yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
+yum install mono-complete
