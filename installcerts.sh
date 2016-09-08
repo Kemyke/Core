570cb6d52a0776edefec416e7c2bd2a5ea62707e
Kemyke <kemyyy@gmail.com>
2016-09-08 08:43:26 +0200

    Add files via upload

--- installcerts.sh
+++ installcerts.sh
@@ -3,9 +3,10 @@
 # Github : kemyke
 # Version : 0.1
 
+
 #Install mono
 mkdir /mono
 wget http://download.mono-project.com/repo/xamarin.gpg -P /mono
 rpm --import /mono/xamarin.gpg
 yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
-yum install mono-complete
+yum install -y mono-complete
